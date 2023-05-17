35f450880f86b3939fc9b815420216144598d633
NorichikaSugii <wawa60379@gmail.com>
2023-05-17 19:40:11 +0900

    保存された買い物記録を表示できるように

--- RealmKakeibo/ViewController.swift
+++ RealmKakeibo/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITableViewDataSource{
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let realm = try! Realm()//Realmを扱うための宣言
+    var items: [ShoppingItem] = []//shoppingItem型の配列を宣言
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //DataSourceを継承したらセットで書く
+        tableView.dataSource = self
+        //カスタムセルをTableViewに登録
+        tableView.register(UINib(nibName: "ItemTableViewCell", bundle: nil), forCellReuseIdentifier: "ItemCell")
+        items = readItems()//取得した買い物の記録データをitemsに代入
     }
-
-
+    
+    //表示するセルの数を指定する　要素数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    //どんな内容のセルを表示するか指定する
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
+        let item: ShoppingItem = items[indexPath.row]//◯行目の買い物データが入る
+        cell.setCell(title: item.title, price: item.price, isMarked: item.isMarked)
+        
+        return cell
+    }
+    
+    //戻り値が[ShoppingItem]型の関数（矢印の後が型）　配列として記録された値を返す
+    func readItems() -> [ShoppingItem] {
+        return Array(realm.objects(ShoppingItem.self))//returnの後が戻り値
+    }
+    
+    
 }
 
